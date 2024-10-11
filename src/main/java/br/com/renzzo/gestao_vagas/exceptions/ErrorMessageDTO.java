2c937c2f3d232324ddfa5e67d18ad3a36ac6fa35
Renzzo Rocha <renzzorocha17@gmail.com>
2024-10-11 10:16:52 -0300

    feat: adding message validation for err

---
+++ src/main/java/br/com/renzzo/gestao_vagas/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,15 @@
+package br.com.renzzo.gestao_vagas.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
+public class ErrorMessageDTO {
+    
+ private String message;
+ private String field;
+
+
+}
